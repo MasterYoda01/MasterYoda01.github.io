a5669b1f31536fbea687ce59341be3bc5d59e404
Joseph <justdoye@mit.edu>
2025-02-15 15:31:33 -0500

    make vercel same as local font

---
+++ build/precache-manifest.487ac1e48edca0e0fc401ce84161ba76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5934cc743b49298f406fd5ef0fdc5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eeb45cdaaba6ffffa4a",
+    "url": "/static/css/2.02e251ca.chunk.css"
+  },
+  {
+    "revision": "7eae0d1510429157d8dd",
+    "url": "/static/css/main.4c25ff6c.chunk.css"
+  },
+  {
+    "revision": "6eeb45cdaaba6ffffa4a",
+    "url": "/static/js/2.8129397a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.8129397a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eae0d1510429157d8dd",
+    "url": "/static/js/main.ff176925.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
